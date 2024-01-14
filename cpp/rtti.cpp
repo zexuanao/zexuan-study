cb7bd657bd4458535b8dd0dc7b980bd2ac60c815
zexuan <3142817283@qq.com>
2024-01-14 11:20:38 +0800

    1.13

--- cpp/rtti.cpp
+++ cpp/rtti.cpp
@@ -71,7 +71,7 @@ int main() {
         cout << "Now processing type " << typeid(*pg).name() << "\n";
         pg->Speak();
 
-        if (ps = dynamic_cast<Superb*>(pg)) {
+        if (ps = dynamic_cast<Superb *>(pg)) {
             ps->Say();
         }
 
