71f7a89c802a8b8ee9e4128203d53ea20ec8618a
zexuan <3142817283@qq.com>
2024-01-14 21:16:06 +0800

    1.14

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<memory>
 using namespace std;
 const int SIZE =5;
 void display(const string sa[],int n);
@@ -9,7 +10,18 @@ double refcube (double &ra);
 void left(const char*,int n=1);
 int main()
 {   
-    
+    using namespace std;
+    string *pu0=new string ("nihao");
+    unique_ptr<string>pu1(new string ("HI ho!"));
+    shared_ptr<string>pu2(pu1);
+    shared_ptr<string>pu3(new string ("nihao"));
+    pu2=pu3;
+    cout<<*pu0;
+    cout<<*pu1;
+    cout<<*pu2;
+    cout<<*pu3;
+    delete pu0;
+
 }
 void display(const string sa[],int n)
 {
