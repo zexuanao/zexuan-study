f90058e020cddce6cd0cd6b24426bc210605640f
zexuan <3142817283@qq.com>
2024-02-08 20:20:05 +0800

    2.8

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 
+
+
+int main()
+{
+    std::cout<< __cplusplus;
+    print(7.5,"hello",42);
+}
 void print(){}
 
 template<typename T,typename... Types>
 void print(const T& firstArg,const Types&... args)
 {
-    cout << firstArg<<endl;
+    std::cout << firstArg<<std::endl;
     print(args...);
-}
-int main()
-{
-    std::cout<< __cplusplus;
-    print(7.5,"hello",42);
 }
