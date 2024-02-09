a8bafbc87445f61194a3696a5fea0f956701351d
zexuan <3142817283@qq.com>
2024-02-10 00:19:57 +0800

    2.10

--- cpp/container.cpp
+++ cpp/container.cpp
@@ -7,19 +7,25 @@
 #define ASIZE 500000
 
 using namespace std;
-namespace np_array {
+namespace np_array 
+{
     void test_array();
     long get_a_target_long();
     int compareLongs(const void*a,const void* b);
 }
-
+namespace np_vector
+{
+    void test_vector(const long& value);
+    string get_a_target_string();
+}
 
 
 
 
 int main()
 {   
-    np_array::test_array();
+    
+    np_vector::test_vector(ASIZE);
     return 0;
 }
 
@@ -32,18 +38,18 @@ namespace np_array
     {
         srand(time(NULL));
         cout<<"\ntest_array().........\n";
-        array<long,ASIZE> c;
-        clock_t timestart=clock();
+    array<long,ASIZE> c;
+    clock_t timestart=clock();
         for(long i=0;i<ASIZE;++i)
         {
             c[i]=rand();
         }
         cout<<"m-second:"<<(clock()-timestart)<<endl;
         cout<<c.size()<<endl<<c.front()<<endl<<c.back()<<endl<<c.data()<<endl;
-        long target=get_a_target_long();
+    long target=get_a_target_long();
         timestart=clock();
         qsort(c.data(),ASIZE,sizeof(long),compareLongs);
-        long* pItem=(long*)bsearch(&target,c.data(),ASIZE,sizeof(long),compareLongs);
+    long* pItem=(long*)bsearch(&target,c.data(),ASIZE,sizeof(long),compareLongs);
         cout<<"qsort+bsearch:"<<(clock()-timestart)<<endl;
         if(pItem!=NULL)
             cout<<"foud"<<*pItem<<endl;
@@ -52,20 +58,60 @@ namespace np_array
     }
     long get_a_target_long()
     {
-        long target=0;
+    long target=0;
         cout<<"target:";
         cin>>target;
         return target;
 
-    }
-    string get_a_target_string()
-    {
-        string target;
-        cin>>target;
-        return target;
     }   
     int compareLongs(const void*a,const void* b)
     {
         return (*(long*)a-*(long*)b);
     }
+}
+#include<vector>
+#include<algorithm>
+namespace np_vector
+{
+    void test_vector(const long& value)
+    {
+        srand(time(NULL));
+        cout<<"\ntest_vector()......\n";
+    vector<string>c;
+    clock_t timestart=clock();
+        for(long i=0;i<value;i++)
+        {
+            c.push_back(to_string(rand()));
+        }
+        cout<<"m-deconds:"<<clock()-timestart<<endl<<"size:"<<c.size()
+            <<endl<<"front:"<<c.front()<<endl<<"back"<<c.back()<<endl<<"data"<<c.data()<<endl<<
+            "capacity"<<c.capacity()<<endl;
+    string target=get_a_target_string();
+        timestart=clock();
+    auto pItem=::find(c.begin(),c.end(),target);
+        cout<<"::find(),m-s:"<<clock()-timestart<<endl;
+        if(pItem!=c.end())
+            cout<<"found "<<*pItem<<endl;
+        else
+            cout<<"not found"<<endl;
+        
+        timestart=clock();
+        sort(c.begin(),c.end());
+    string* pItem2=(string*)bsearch(&target,c.data(),c.size(),sizeof(string),[](const void* a, const void* b) -> int {
+        return (*(string*)a).compare(*(string*)b);
+    });
+        cout <<"sort()+bsearch(),m-s:"<<(clock()-timestart)<<endl;
+        if(pItem2 !=NULL)
+            cout<<"found,"<<*pItem2<<endl;
+        else
+            cout<<"not found!"<<endl;
+
+    }
+    string get_a_target_string()
+    {
+    string target;
+        cin>>target;
+        return target;
+    }
+      
 }
--- python/test.py
+++ python/test.py
@@ -1,2 +1,19 @@
-with open(".\\a.txt") as f:
-    print(f.read())
+import requests
+from bs4 import BeautifulSoup
+head={ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
+response=requests.get("https://movie.douban.com/chart", headers=head)
+html=response.text
+soup=BeautifulSoup(html,"html.parser")
+all_titles=soup.findAll("div",attrs={"class":"pl2"})
+for title in all_titles:
+    all_links=title.findAll("a")
+    for link in all_links:
+        text = link.get_text(strip=True)
+        slash_index = text.find('/')
+        first_chinese_text = text[:slash_index].strip()
+        print(first_chinese_text)
+        
+        
+        #keys=link.findAll("span",attrs={"style":"font-size:13px;"})
+        #for key in keys:
+        #    print(key.string)
